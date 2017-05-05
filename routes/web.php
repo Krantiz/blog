810416c6354fdd1901e11ed3e94e2758ff322d76
Krantiz <kranti.gajula@gmail.com>
2017-05-05 12:48:48 +0530

    Login dashboard added

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('names', function()
+{
+    return array(
+      1 => "John",
+      2 => "Mary",
+      3 => "Steven"
+    );
+});
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
